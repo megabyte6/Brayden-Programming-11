fb02811d452028101ad83961984f1b653616d7dc
Brayden <72172597+megabyte6@users.noreply.github.com>
2020-11-13 17:29:59 -0800

    Add files via upload

---
+++ AsciiArt.java
@@ -0,0 +1,28 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+/*
+    (  )   (   )  )
+     ) (   )  (  (
+     ( )  (    ) )
+     _____________
+    <_____________> ___
+    |             |/ _ \
+    |               | | |
+    |               |_| |
+ ___|             |\___/
+/    \___________/    \
+\_____________________/
+*/
+        System.out.println("    (  )   (   )  )");
+        System.out.println("     ) (   )  (  (");
+        System.out.println("     ( )  (    ) )");
+        System.out.println("     _____________");
+        System.out.println("    <_____________> ___");
+        System.out.println("    |             |/ _ \\");
+        System.out.println("    |               | | |");
+        System.out.println("    |               |_| |");
+        System.out.println(" ___|             |\\___/");
+        System.out.println("/    \\___________/    \\");
+        System.out.println("\\_____________________/");
+    }
+}
