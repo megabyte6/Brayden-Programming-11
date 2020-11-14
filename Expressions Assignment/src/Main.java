5c497a6ea4b87d2d93dc946fd08d1daf0b3e590d
Brayden <72172597+megabyte6@users.noreply.github.com>
2020-11-13 17:33:20 -0800

    Update Main.java

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -15,12 +15,14 @@ public class Main {
         */
         //Expression 1
         System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        //Answer
         System.out.println(5 + 10/2 + 4/3);
         System.out.println(5 + 5 + 1);
         System.out.println(11);
 
         //Expression 2
         System.out.println(23%4 + (18 - 3 / 2));
+        //Answer
         System.out.println(23%4 + (18 - 1));
         System.out.println(23%4 + 17);
         System.out.println(3 + 17);
@@ -28,6 +30,7 @@ public class Main {
 
         //Expression 3
         System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        //Answer
         System.out.println(3 + Math.pow(3,4)/5);
         System.out.println(3 + 81.0 / 5);
         System.out.println(3 + 16.2);
@@ -35,6 +38,7 @@ public class Main {
 
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        //Answer
         System.out.println(1 - 8 + Math.pow(2,3) / 8 + (9.0 / 3));
         System.out.println(1 - 8 + Math.pow(2,3) / 8 + 3.0);
         System.out.println(1 - 8 + 8 / 8 + 3.0);
@@ -43,6 +47,7 @@ public class Main {
 
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        //Answer
         System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((10 * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((10 * 0.8509035245341184 + 2) - Math.pow(Math.sqrt(25), 3));
