89252c2df8c8034e435a9e7cf0ddbf73bb464cfe
megabyte6 <braydenmchan@gmail.com>
2021-03-23 18:55:50 -0700

    Add javafx

--- Unit_6/GuiLesson/src/sample/Controller.java
+++ Unit_6/GuiLesson/src/sample/Controller.java
@@ -1,5 +1,5 @@
 package sample;
 
 public class Controller {
-    // Controller code
+    // Code for controller
 }
--- Unit_6/GuiLesson/src/sample/Main.java
+++ Unit_6/GuiLesson/src/sample/Main.java
@@ -7,15 +7,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setTitle("GUI Lesson");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 }
