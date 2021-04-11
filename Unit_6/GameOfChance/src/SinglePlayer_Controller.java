5053942610b7bdd539c82126c4cbc7961e857754
megabyte6 <braydenmchan@gmail.com>
2021-04-11 07:15:54 -0700

    Add button to go to start screen

--- Unit_6/GameOfChance/src/SinglePlayer_Controller.java
+++ Unit_6/GameOfChance/src/SinglePlayer_Controller.java
@@ -1,3 +1,27 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class SinglePlayer_Controller {
-    
+    @FXML
+    private Button button_back;
+
+    public void returnHome(ActionEvent actionEvent) {
+        // Close current window
+        Stage currentStage = (Stage) button_back.getScene().getWindow();
+        currentStage.close();
+
+        // Try to open new window
+        try {
+            Stage homeStage = new Stage();
+            homeStage.setTitle("Guess the Number");
+            homeStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Start.fxml"))));
+            homeStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
