9831664d1ae99c7af08fe2b5b6f86ddad35b9921
megabyte6 <braydenmchan@gmail.com>
2021-04-11 11:06:35 -0700

    Create files for hard level

---
+++ Unit_6/GameOfChance/src/GameUIs/Hard/Hard_Controller.java
@@ -0,0 +1,29 @@
+package GameUIs.Hard;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class Hard_Controller {
+    @FXML
+    private Button button_back;
+
+    public void returnHome(ActionEvent actionEvent) {
+        // Close current window
+        Stage currentStage = (Stage) button_back.getScene().getWindow();
+        currentStage.close();
+
+        // Try to open new window
+        try {
+            Stage homeStage = new Stage();
+            homeStage.setTitle("Bingo");
+            homeStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Start.fxml"))));
+            homeStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
