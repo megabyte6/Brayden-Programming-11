23e04b28c3f58039e81ffb0f60b28c0ab7ed00e2
Brayden <braydenmchan@gmail.com>
2021-04-14 21:26:18 -0700

    Remove unused methods

--- Unit_6/GameOfChance/src/Easy_Controller.java
+++ Unit_6/GameOfChance/src/Easy_Controller.java
@@ -47,19 +47,6 @@ public class Easy_Controller implements Initializable {
         });
     }
 
-    public Stage openWindow(String fxmlFile, String titleName) {
-        // Try to open new window
-        Stage newStage = new Stage();
-        newStage.setTitle(titleName);
-        try {
-            newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
-            newStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return newStage;
-    }
-
     public Stage openWindow(String fxmlFile, String titleName, Stage currentStage) {
         // Close old window
         currentStage.close();
