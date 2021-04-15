233964dd5865f893d33a255cc36f752bec40bbef
Brayden <braydenmchan@gmail.com>
2021-04-14 21:08:49 -0700

    Add method for changing full screen state

--- Unit_6/GameOfChance/src/StartController.java
+++ Unit_6/GameOfChance/src/StartController.java
@@ -8,10 +8,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class StartController implements Initializable {
     // Variables represent elements in the UI
+    @FXML
+    private AnchorPane anchorPane_root;
     @FXML
     private ComboBox<String> comboBox_gameMode;
     @FXML
@@ -31,48 +36,86 @@ public class StartController implements Initializable {
         comboBox_gameMode.getSelectionModel().selectFirst();
     }
 
-    // Returns 0 if completed successfully else returns 1
-    public int openWindow(String fxmlFile, Stage currentStage, String titleName) {
-        // Create variable for returning error number
-        int errorNum = 0;
-        
-        // Close old window
-        currentStage.close();
-
+    public Stage openWindow(String fxmlFile, String titleName) {
         // Try to open new window
+        Stage newStage = new Stage();
+        newStage.setTitle(titleName);
         try {
-            Stage newStage = new Stage();
-            newStage.setTitle(titleName);
             newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
             newStage.show();
         } catch (Exception e) {
             e.printStackTrace();
-            errorNum = 1;
         }
-        return errorNum;
+        return newStage;
+    }
+
+    public Stage openWindow(String fxmlFile, String titleName, Stage currentStage) {        
+        // Close old window
+        currentStage.close();
+
+        // Try to open new window
+        Stage newStage = new Stage();
+        newStage.setTitle(titleName);
+        try {
+            newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
+            newStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newStage;
+    }
+
+    public void processKey(KeyEvent key) {
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+
+        // Full screen window when F11 is pressed
+        if (key.getCode() == KeyCode.F11) {
+            if (currentStage.isFullScreen()) {
+                currentStage.setFullScreen(false);
+            } else {
+                currentStage.setFullScreen(true);
+            }
+        }
+    }
+
+    public void setFullScreen() {
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+        if (currentStage.isFullScreen()) {
+            currentStage.setFullScreen(false);
+        } else {
+            currentStage.setFullScreen(true);
+        }
+    }
+
+    public void setFullScreen(boolean fullScreen) {
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+        currentStage.setFullScreen(fullScreen);
     }
 
     // Code for play button
     public void play(ActionEvent actionEvent) throws Exception {
-        Stage currentStage = (Stage) button_play.getScene().getWindow();
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
 
         // Check if easy, medium, or hard mode is selected
         if ("Easy".equals(comboBox_gameMode.getValue())) {
-            openWindow("Easy.fxml", currentStage, "Bingo");
+            openWindow("Easy.fxml", "Bingo", currentStage);
         } else if ("Medium".equals(comboBox_gameMode.getValue())) {
-            openWindow("Medium.fxml", currentStage, "Bingo");
+            openWindow("Medium.fxml", "Bingo", currentStage);
         } else if ("Hard".equals(comboBox_gameMode.getValue())) {
-            openWindow("Hard.fxml", currentStage, "Bingo");
+            openWindow("Hard.fxml", "Bingo", currentStage);
         }
     }
 
     public void openInstructions(ActionEvent actionEvent) throws Exception {
-        Stage currentStage = (Stage) button_instructions.getScene().getWindow();
-        openWindow("Instructions.fxml", currentStage, "Instructions");
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+        Stage newStage = openWindow("Instructions.fxml", "Instructions", currentStage);
+        if (currentStage.isFullScreen()) {
+            newStage.setFullScreen(true);
+        }
     }
 
     public void openScoreBoard(ActionEvent actionEvent) throws Exception {
-        Stage currentStage = (Stage) button_scoreBoard.getScene().getWindow();
-        openWindow("HighScores.fxml", currentStage, "Score Board");
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+        openWindow("HighScores.fxml", "Score Board", currentStage);
     }
 }
