4a4ed66b7aeb15d23b93803e079f5d7da29c769a
Brayden <braydenmchan@gmail.com>
2021-04-14 21:14:59 -0700

    Remove unused methods

--- Unit_6/GameOfChance/src/Instructions_Controller.java
+++ Unit_6/GameOfChance/src/Instructions_Controller.java
@@ -9,19 +9,6 @@ public class Instructions_Controller {
     @FXML
     private Button button_back;
 
-    public Stage openWindow(String fxmlFile, String titleName) {
-        // Try to open new window
-        Stage newStage = new Stage();
-        newStage.setTitle(titleName);
-        try {
-            newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
-            newStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return newStage;
-    }
-
     public Stage openWindow(String fxmlFile, String titleName, Stage currentStage) {        
         // Close old window
         currentStage.close();
