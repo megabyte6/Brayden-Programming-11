d18fc17b95099397b1251ef2af599e9a8f379546
Brayden <braydenmchan@gmail.com>
2021-04-17 16:09:43 -0700

    Create overlay for start of game

--- Unit_6/GameOfChance/src/Easy_Controller.java
+++ Unit_6/GameOfChance/src/Easy_Controller.java
@@ -1,20 +1,17 @@
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class Easy_Controller implements Initializable {
+public class Easy_Controller {
     // Goes [col][row]
     private Node[][] playerCells = new Node[5][5];
     private Node[][] computerCells = new Node[5][5];
@@ -22,49 +19,87 @@ public class Easy_Controller implements Initializable {
     private boolean[][] computerCellStates = new boolean[5][5];
 
     @FXML
-    private GridPane gridPane_root;
+    AnchorPane anchorPane_root;
     @FXML
-    private GridPane gridPane_player;
+    GridPane gridPane_player;
     @FXML
-    private GridPane gridPane_computer;
+    GridPane gridPane_computer;
     @FXML
-    private Button button_back;
+    Button button_back;
+    @FXML
+    Button button_overlay;
+    
+    public Stage openWindow(String fxmlFile, String titleName, Stage currentStage) {
+        // Close old window
+        currentStage.close();
+        
+        // Try to open new window
+        Stage newStage = new Stage();
+        newStage.setTitle(titleName);
+        try {
+            newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
+            newStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newStage;
+    }
+    
+    public void setFullScreen() {
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+        if (currentStage.isFullScreen()) {
+            currentStage.setFullScreen(false);
+        } else {
+            currentStage.setFullScreen(true);
+        }
+    }
+    
+    public void setFullScreen(boolean fullScreen) {
+        Stage currentStage = (Stage) anchorPane_root.getScene().getWindow();
+        currentStage.setFullScreen(fullScreen);
+    }
+    
+    public void returnHome(ActionEvent actionEvent) {
+        Stage currentStage = (Stage) button_back.getScene().getWindow();
+        openWindow("Start.fxml", "Bingo", currentStage);
+    }
 
-    // Run when Easy_Controller.java is initialized
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initializeGameBoard() {
+        System.out.println("Initialization started");
         // Initialize a multidimensional array for every node on the player's bingo card
         for (Node cell : gridPane_player.getChildren()) {
-            System.out.println(cell.toString());
             playerCells[GridPane.getColumnIndex(cell)][GridPane.getRowIndex(cell)] = (Label) cell;
         }
-
+    
         // Initialize a multidimensional array for every node on the computer's bingo card
         for (Node cell : gridPane_computer.getChildren()) {
             computerCells[GridPane.getColumnIndex(cell)][GridPane.getRowIndex(cell)] = cell;
         }
-
+    
         // Create array to store states of the player's and computer's cells
         for (int col = 0; col < 5; col++) {
             for (int row = 0; row < 5; row++) {
                 playerCellStates[col][row] = false;
                 computerCellStates[col][row] = false;
-
+    
+                final int tempCol = col;
+                final int tempRow = row;
+    
                 playerCells[col][row].addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>(){
                     @Override
                     public void handle(MouseEvent mouseEvent) {
                         if (playerCellStates[1][1] == true) {
-                            ((Label) playerCells[col][row]).setStyle("-fx-border-color: black; -fx-background-color: transparent");
+                            ((Label) playerCells[tempCol][tempRow]).setStyle("-fx-border-color: black; -fx-background-color: transparent");
                             playerCellStates[1][1] = false;
                         } else {
-                            playerCells[col][row].setStyle("-fx-border-color: black; -fx-background-color: lightgrey");
+                            playerCells[tempCol][tempRow].setStyle("-fx-border-color: black; -fx-background-color: lightgrey");
                             playerCellStates[1][1] = true;
                         }
                     }
                 });
             }
         }
-
+        
         // Add event handlers
         // for (int col = 0; col < 5; col++) {
         //     for (int row = 0; row < 5; row++) {
@@ -82,40 +117,5 @@ public class Easy_Controller implements Initializable {
         //         });
         //     }
         // }
-    }
-
-    public Stage openWindow(String fxmlFile, String titleName, Stage currentStage) {
-        // Close old window
-        currentStage.close();
-
-        // Try to open new window
-        Stage newStage = new Stage();
-        newStage.setTitle(titleName);
-        try {
-            newStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
-            newStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return newStage;
-    }
-
-    public void setFullScreen() {
-        Stage currentStage = (Stage) gridPane_root.getScene().getWindow();
-        if (currentStage.isFullScreen()) {
-            currentStage.setFullScreen(false);
-        } else {
-            currentStage.setFullScreen(true);
-        }
-    }
-
-    public void setFullScreen(boolean fullScreen) {
-        Stage currentStage = (Stage) gridPane_root.getScene().getWindow();
-        currentStage.setFullScreen(fullScreen);
-    }
-
-    public void returnHome(ActionEvent actionEvent) {
-        Stage currentStage = (Stage) button_back.getScene().getWindow();
-        openWindow("Start.fxml", "Bingo", currentStage);
     }
 }
