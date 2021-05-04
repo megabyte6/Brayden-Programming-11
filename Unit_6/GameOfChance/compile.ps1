3a9845f61a3bfb50e09ca0fb48064cf5a4797182
Brayden <braydenmchan@gmail.com>
2021-05-03 17:51:16 -0700

    Accept new folder structure

--- Unit_6/GameOfChance/compile.ps1
+++ Unit_6/GameOfChance/compile.ps1
@@ -1,3 +1,3 @@
 & 'C:\Program Files\Java\jdk-1.8.0_281\bin\javac.exe' .\src\DataStore.java .\src\Easy_Controller.java .\src\Hard_Controller.java .\src\Instructions_Controller.java .\src\Main.java .\src\Medium_Controller.java .\src\Start_Controller.java
-Move-Item  -Path '.\src\*.class' -Destination '.\bin\' -Force
-& 'C:\Program Files\Java\jdk-16\bin\jar.exe' -c -f .\BingoWithJavaFX.jar -m .\MANIFEST.MF -C .\src . -C .\bin .
+Move-Item  -Path '.\src\*.class' -Destination '.\bin\compiled\' -Force
+& 'C:\Program Files\Java\jdk-16\bin\jar.exe' -c -f .\bin\standalone-application\BingoWithJavaFX.jar -m .\MANIFEST.MF -C .\src\ . -C .\bin\compiled\ .
--- Unit_6/GameOfChance/run.ps1
+++ Unit_6/GameOfChance/run.ps1
@@ -1,1 +1,1 @@
-& 'C:\Program Files\Java\jdk-1.8.0_281\bin\java.exe' -jar .\BingoWithJavaFX.jar
+& 'C:\Program Files\Java\jdk-1.8.0_281\bin\java.exe' -jar .\bin\standalone-application\BingoWithJavaFX.jar
