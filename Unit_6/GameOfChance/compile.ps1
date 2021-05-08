c87ad4784b2de03dc9db0c2f99c122ed59f92bb5
Brayden <braydenmchan@gmail.com>
2021-05-08 12:15:23 -0700

    Update for usage of the latest version of java

--- Unit_6/GameOfChance/compile.ps1
+++ Unit_6/GameOfChance/compile.ps1
@@ -1,3 +1,13 @@
+# Compile all classes using java 8 and put them in the bin folder
+# This first step is to make sure that the jar file can run as JavaFX jar files no longer have
+# support from later versions of java
 & 'C:\Program Files\Java\jdk-1.8.0_281\bin\javac.exe' .\src\DataStore.java .\src\Easy_Controller.java .\src\Hard_Controller.java .\src\Instructions_Controller.java .\src\Main.java .\src\Medium_Controller.java .\src\Start_Controller.java
-Move-Item  -Path '.\src\*.class' -Destination '.\bin\compiled\' -Force
-& 'C:\Program Files\Java\jdk-16\bin\jar.exe' -c -f .\bin\standalone-application\BingoWithJavaFX.jar -m .\MANIFEST.MF -C .\src\ . -C .\bin\compiled\ .
+Move-Item -Path '.\src\*.class' -Destination '.\bin\compiled\' -Force
+Copy-Item -Path '.\src\*.fxml' -Destination '.\bin\compiled\' -Force
+
+# Create a jar file with the classes compiled with java 8
+jar -c -f .\bin\standalone-application\BingoWithJavaFX.jar -m .\MANIFEST.MF -C .\bin\compiled\ .
+
+# Recompile classes using the latest version of java and put them in the bin folder
+javac --module-path ..\javafx-sdk-16\lib\ --add-modules javafx.controls,javafx.fxml .\src\DataStore.java .\src\Easy_Controller.java .\src\Hard_Controller.java .\src\Instructions_Controller.java .\src\Main.java .\src\Medium_Controller.java .\src\Start_Controller.java
+Move-Item -Path '.\src\*.class' -Destination '.\bin\compiled\' -Force
--- Unit_6/GameOfChance/run.ps1
+++ Unit_6/GameOfChance/run.ps1
@@ -1,1 +1,12 @@
-& 'C:\Program Files\Java\jdk-1.8.0_281\bin\java.exe' -jar .\bin\standalone-application\BingoWithJavaFX.jar
+$pathToJava8 = 'C:\Program Files\Java\jdk-1.8.0_281\bin\java.exe'
+
+# Check if the user wants to run the standalone applicaton (jar file) or not
+if (($args[0]) -and ($args[0] -eq 'app')) {
+    if (Test-Path -Path $pathToJava8) {
+         & $pathToJava8 -jar .\bin\standalone-application\BingoWithJavaFX.jar
+    } else {
+        Write-Host "Error: the file '$pathToJava8' doesn't seem to exist"
+    }
+} else {
+    java --module-path ..\javafx-sdk-16\lib\ --add-modules javafx.controls,javafx.fxml -cp .\bin\compiled\ Main
+}
