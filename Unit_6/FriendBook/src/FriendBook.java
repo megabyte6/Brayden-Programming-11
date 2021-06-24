5b2fded18efc02075ad813c8dcded37c7a00f22a
Brayden Chan <braydenmchan@gmail.com>
2021-06-24 07:52:11 -0700

    Bug fixes

--- Unit_6/FriendBook/src/FriendBook.java
+++ Unit_6/FriendBook/src/FriendBook.java
@@ -1,22 +1,29 @@
+import static java.lang.System.out;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.function.Supplier;
+
+import com.github.megabyte6.utils.javafx.InputBox;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class FriendBook implements Initializable {
 
+    private final Supplier<String[]> groupIDs = () ->
+            FriendDatabase.getStringArray("groupIDs");
     private final Supplier<String> friendGroupID = () ->
             FriendDatabase.getString("groupID");
     private final Supplier<Friend[]> friendGroup = () ->
@@ -24,8 +31,12 @@ public class FriendBook implements Initializable {
     private final Supplier<Integer> friendGroupSize = () ->
             FriendDatabase.friendGroupSize(friendGroupID.get());
 
+    private boolean refreshListenerAdded = false;
+
     @FXML
-    private ListView<Friend> listView_friendList = new ListView<Friend>();
+    private ComboBox<String> selectGroup;
+    @FXML
+    private ListView<Friend> friendList;
 
     @FXML
     private Label label_name;
@@ -44,21 +55,33 @@ public class FriendBook implements Initializable {
     public void initialize(URL arg0, ResourceBundle arg1) {
         FriendDatabase.setInteger("friendIndex", -1);
 
+        // Initialize ComboBox
+        for (String item : groupIDs.get()) {
+            selectGroup.getItems().add(item);
+        }
+        selectGroup.getSelectionModel().selectFirst();
+
         // Initialize ListView
         for (int i = 0; i < friendGroupSize.get(); i++) {
-            listView_friendList.getItems().add(
+            friendList.getItems().add(
                     FriendDatabase.getFriend(friendGroupID.get(), i));
         }
-        listView_friendList.getSelectionModel().selectFirst();
+        friendList.getSelectionModel().selectFirst();
 
         displayInfo();
     }
 
     private Stage openWindow(String fxmlFile, String titleName) {
+        // Check if the refresh listener was set
+         if (!refreshListenerAdded) {
+            Stage currentStage = (Stage) friendList.getScene().getWindow();
+            currentStage.focusedProperty().addListener(
+                    (observable, oldNode, newNode) -> refresh());
+            refreshListenerAdded = true;
+        }
+
         Stage tempStage = new Stage();
         tempStage.setTitle(titleName);
-
-        // Try to get fxml file for UI
         try {
             tempStage.setScene(new Scene(FXMLLoader.load(getClass().getResource(fxmlFile))));
         } catch (Exception e) {
@@ -70,7 +93,13 @@ public class FriendBook implements Initializable {
     }
 
     public void processKey(KeyEvent key) {
-        if (key.isControlDown()) {
+        if (key.isControlDown() && key.isShiftDown()) {
+            if (key.getCode() == KeyCode.N) {
+                createGroup();
+            } else if (key.getCode() == KeyCode.E) {
+                renameGroup();
+            }
+        } else if (key.isControlDown()) {
             if (key.getCode() == KeyCode.R) {
                 refresh();
             } else if (key.getCode() == KeyCode.N) {
@@ -78,40 +107,70 @@ public class FriendBook implements Initializable {
             } else if (key.getCode() == KeyCode.E) {
                 editFriend();
             }
+        } else if (key.isShiftDown()) {
+            if (key.getCode() == KeyCode.DELETE) {
+                deleteGroup();
+            }
         } else if (key.getCode() == KeyCode.DELETE) {
             deleteFriend();
         } 
     }
 
     public void refresh() {
+        out.println("Refresh");
+
+        // Check if the ComboBox and array of IDs are out of sync
+        if (selectGroup.getItems().size() != groupIDs.get().length) {
+            selectGroup.getItems().clear();
+            for (int i = 0; i < groupIDs.get().length; i++) {
+                selectGroup.getItems().add(groupIDs.get()[i]);
+            }
+        } else {
+            for (int i = 0; i < groupIDs.get().length; i++) {
+                // Check if the the items in selectGroup match the database
+                if (!selectGroup.getItems().get(i).equals(groupIDs.get()[i])) {
+                    selectGroup.getItems().set(i, groupIDs.get()[i]);
+                }
+            }
+        }
+        selectGroup.getSelectionModel().select(friendGroupID.get());
+
         // Check if the ListView and array of Friend objects are out of sync
-        // Should not happen normally
-        if (listView_friendList.getItems().size() != friendGroupSize.get()) {
+        if (friendList.getItems().size() != friendGroupSize.get()) {
             // Reload all items
-            listView_friendList.getItems().clear();
+            friendList.getItems().clear();
             for (int i = 0; i < friendGroupSize.get(); i++) {
-                listView_friendList.getItems().add(
+                friendList.getItems().add(
                         FriendDatabase.getFriend(friendGroupID.get(), i));
             }
-            return;
-        }
-        for (int i = 0; i < friendGroupSize.get(); i++) {
-            if (!listView_friendList.getItems().get(i)
-                    .equals(FriendDatabase.getFriend(friendGroupID.get(), i))) {
-                listView_friendList.getItems().set(i,
-                        FriendDatabase.getFriend(friendGroupID.get(), i));
+        } else {
+            for (int i = 0; i < friendGroupSize.get(); i++) {
+                // Check if the items displayed match the database
+                if (!friendList.getItems().get(i)
+                        .equals(FriendDatabase.getFriend(friendGroupID.get(), i))) {
+                    friendList.getItems().set(i,
+                            FriendDatabase.getFriend(friendGroupID.get(), i));
+                }
             }
         }
+
+        // Check which friend should be selected
+        if (FriendDatabase.getInteger("friendIndex") != -1) {
+            friendList.getSelectionModel().select(FriendDatabase.getInteger("friendIndex"));
+        }
+
+        displayInfo();
     }
 
     public void displayInfo() {
-        Friend friend = listView_friendList.getSelectionModel().getSelectedItem();
-        if (listView_friendList.getSelectionModel().getSelectedIndex() == -1) return;
+        Friend friend = friendList.getSelectionModel().getSelectedItem();
+        if (friendList.getSelectionModel().getSelectedIndex() == -1) return;
+
         // Check if the values were set and if so, display them
-        if (friend.isNameSet()) label_name.setText(friend.getName());
-        if (friend.isAgeSet()) label_age.setText(String.valueOf(friend.getAge()));
+        label_name.setText(friend.getName());
+        label_age.setText(friend.isAgeSet() ? String.valueOf(friend.getAge()) : "");
+        String birthDate = "";
         if (friend.isBirthDateSet()) {
-            String birthDate = "";
             switch (friend.getBirthMonth()) {
                 case 1: birthDate += "Jan "; break;
                 case 2: birthDate += "Feb "; break;
@@ -128,17 +187,20 @@ public class FriendBook implements Initializable {
             }
             birthDate += friend.getBirthDay() + ", ";
             birthDate += friend.getBirthYear();
-
-            label_birthday.setText(birthDate);
         }
+        label_birthday.setText(birthDate);
         if (friend.isHeightSet()) {
             label_height.setText(friend.getHeightUnit().equals("cm")
                     ? friend.getHeight("cm") + " cm"
-                    : friend.getHeight("in") + " inches");
-        }
-        if (friend.isGenderSet()) label_gender.setText(friend.getGender());
-        if (friend.isOtherInfoSet()) label_otherInfo.setText(friend.getOtherInfo());
+                    : friend.getHeight("in") + " Inches");
+        } else label_height.setText("");
+        label_gender.setText(friend.getGender());
+        label_otherInfo.setText(friend.getOtherInfo());
     }
+
+
+
+
 
     public void createFriend() {
         FriendDatabase.changeInteger("friendIndex", -1);
@@ -146,7 +208,7 @@ public class FriendBook implements Initializable {
     }
 
     public void editFriend() {
-        int selectedIndex = listView_friendList.getSelectionModel().getSelectedIndex();
+        int selectedIndex = friendList.getSelectionModel().getSelectedIndex();
         if (selectedIndex == -1) {
             new Alert(AlertType.NONE, "Please select a friend to edit", ButtonType.OK)
                     .showAndWait();
@@ -157,8 +219,8 @@ public class FriendBook implements Initializable {
     }
 
     public void deleteFriend() {
-        int selectedFriendIndex = listView_friendList.getSelectionModel().getSelectedIndex();
-        int newSelectedIndex = listView_friendList.getItems().size() - 2;
+        int selectedFriendIndex = friendList.getSelectionModel().getSelectedIndex();
+        int newSelectedIndex = friendList.getItems().size() - 2;
         if (selectedFriendIndex == -1) {
             new Alert(AlertType.NONE, "Please select a friend to remove", ButtonType.OK)
                     .showAndWait();
@@ -172,14 +234,14 @@ public class FriendBook implements Initializable {
         deleteFriend.showAndWait();
 
         if (deleteFriend.getResult() == ButtonType.YES) {
-            listView_friendList.getSelectionModel().select(newSelectedIndex);
-            listView_friendList.getItems().remove(selectedFriendIndex);
+            friendList.getSelectionModel().select(newSelectedIndex);
+            friendList.getItems().remove(selectedFriendIndex);
             FriendDatabase.removeFriend(friendGroupID.get(), selectedFriendIndex);
         }
     }
 
     public void moveItemUp() {
-        int selectedIndex = listView_friendList.getSelectionModel().getSelectedIndex();
+        int selectedIndex = friendList.getSelectionModel().getSelectedIndex();
         if (selectedIndex == -1) {
             new Alert(AlertType.NONE, "Select a friend to move", ButtonType.OK)
                     .showAndWait();
@@ -188,19 +250,52 @@ public class FriendBook implements Initializable {
         Friend removedFriend = FriendDatabase.removeFriend(friendGroupID.get(), selectedIndex);
         FriendDatabase.insertFriend(removedFriend, friendGroupID.get(), selectedIndex - 1);
         refresh();
-        listView_friendList.getSelectionModel().select(selectedIndex - 1);
+        friendList.getSelectionModel().select(selectedIndex - 1);
     }
 
     public void moveItemDown() {
-        int selectedIndex = listView_friendList.getSelectionModel().getSelectedIndex();
+        int selectedIndex = friendList.getSelectionModel().getSelectedIndex();
         if (selectedIndex == -1) {
             new Alert(AlertType.NONE, "Select a friend to move", ButtonType.OK)
                     .showAndWait();
             return;
-        } else if (selectedIndex == listView_friendList.getItems().size() - 1) return;
+        } else if (selectedIndex == friendList.getItems().size() - 1) return;
         Friend removedFriend = FriendDatabase.removeFriend(friendGroupID.get(), selectedIndex);
         FriendDatabase.insertFriend(removedFriend, friendGroupID.get(), selectedIndex + 1);
         refresh();
-        listView_friendList.getSelectionModel().select(selectedIndex + 1);
+        friendList.getSelectionModel().select(selectedIndex + 1);
     }
+
+    public void selectGroup() {
+        if (selectGroup.getSelectionModel().getSelectedItem() == null) return;
+        FriendDatabase.changeString("groupID",
+                selectGroup.getSelectionModel().getSelectedItem());
+        if (friendGroupSize.get() > 0) {
+            FriendDatabase.changeInteger("friendIndex", 0);
+        }
+        refresh();
+    }
+    
+    public void createGroup() {
+        InputBox inputBox = new InputBox("Enter a name for the new group:", "New Friend Group");
+        inputBox.setHeight(175);
+        inputBox.setWidth(300);
+        inputBox.show();
+        String result = inputBox.getResult();
+
+        if (result.equals("")) return;
+
+        String[] tempArray = new String[groupIDs.get().length + 1];
+        for (int i = 0; i < groupIDs.get().length; i++) {
+            tempArray[i] = groupIDs.get()[i];
+        }
+        tempArray[tempArray.length - 1] = result;
+        FriendDatabase.changeStringArray("groupIDs", tempArray);
+
+        FriendDatabase.changeString("groupID", result);
+    }
+
+    public void renameGroup() {}
+
+    public void deleteGroup() {}
 }
